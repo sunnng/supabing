c08989d8600ab435fa2a4be812cf40f72c851561
sunnng <61936492+sunnng@users.noreply.github.com>
2025-05-07 20:57:16 +0800

    fix: better auth

---
+++ drizzle/0000_nifty_silk_fever.sql
@@ -0,0 +1,54 @@
+CREATE TABLE `supabing_account` (
+	`id` varchar(36) NOT NULL,
+	`account_id` text NOT NULL,
+	`provider_id` text NOT NULL,
+	`user_id` text NOT NULL,
+	`access_token` text,
+	`refresh_token` text,
+	`id_token` text,
+	`access_token_expires_at` timestamp,
+	`refresh_token_expires_at` timestamp,
+	`scope` text,
+	`password` text,
+	`created_at` timestamp NOT NULL,
+	`updated_at` timestamp NOT NULL,
+	CONSTRAINT `supabing_account_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE TABLE `supabing_session` (
+	`id` varchar(36) NOT NULL,
+	`expires_at` timestamp NOT NULL,
+	`token` varchar(255) NOT NULL,
+	`created_at` timestamp NOT NULL,
+	`updated_at` timestamp NOT NULL,
+	`ip_address` text,
+	`user_agent` text,
+	`user_id` text NOT NULL,
+	CONSTRAINT `supabing_session_id` PRIMARY KEY(`id`),
+	CONSTRAINT `supabing_session_token_unique` UNIQUE(`token`)
+);
+--> statement-breakpoint
+CREATE TABLE `supabing_user` (
+	`id` varchar(36) NOT NULL,
+	`name` text NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`email_verified` boolean NOT NULL,
+	`image` text,
+	`created_at` timestamp NOT NULL,
+	`updated_at` timestamp NOT NULL,
+	CONSTRAINT `supabing_user_id` PRIMARY KEY(`id`),
+	CONSTRAINT `supabing_user_email_unique` UNIQUE(`email`)
+);
+--> statement-breakpoint
+CREATE TABLE `supabing_verification` (
+	`id` varchar(36) NOT NULL,
+	`identifier` text NOT NULL,
+	`value` text NOT NULL,
+	`expires_at` timestamp NOT NULL,
+	`created_at` timestamp,
+	`updated_at` timestamp,
+	CONSTRAINT `supabing_verification_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+ALTER TABLE `supabing_account` ADD CONSTRAINT `supabing_account_user_id_supabing_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `supabing_user`(`id`) ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `supabing_session` ADD CONSTRAINT `supabing_session_user_id_supabing_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `supabing_user`(`id`) ON DELETE cascade ON UPDATE no action;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,20 +4,19 @@ import { mysqlTableCreator } from "drizzle-orm/mysql-core";
 import { drizzle } from "drizzle-orm/mysql2";
 import { type Pool, createPool } from "mysql2/promise";
 
-import * as schema from "./schema/auth-schema";
-
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  conn: Pool | undefined;
+  poolConnection: Pool | undefined;
 };
 
-const conn = globalForDb.conn ?? createPool({ uri: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const poolConnection =
+  globalForDb.poolConnection ?? createPool({ uri: env.DATABASE_URL });
+if (env.NODE_ENV !== "production") globalForDb.poolConnection = poolConnection;
 
-export const db = drizzle(conn, { schema, mode: "default" });
+export const db = drizzle({ client: poolConnection });
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
