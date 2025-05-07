cf5f0e4020aec602bd18a756add063e32ec4583a
scy <sunchengyuan0706@hotmail.com>
2025-05-07 23:10:11 +0800

    fix: sign-in-form bug fix

--- src/components/sign-in-form.tsx
+++ src/components/sign-in-form.tsx
@@ -8,19 +8,17 @@ import { Button } from "@/components/ui/button";
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 
 const formSchema = z.object({
 	email: z.string().email(),
-	password: z.string(),
+	password: z.string().min(1),
 });
 
 export function SignInForm() {
@@ -46,8 +44,8 @@ export function SignInForm() {
 				onSubmit={form.handleSubmit(onSubmit)}
 				className={cn("flex flex-col gap-6")}
 			>
-				<div className="flex flex-col items-start gap-2 text-center">
-					<h1 className="font-bold text-2xl">Welcome Back</h1>
+				<div className="mb-4 flex flex-col items-start gap-2 text-center">
+					<h1 className="font-bold text-3xl">Welcome Back</h1>
 					<p className="text-balance text-muted-foreground text-sm">
 						Enter your email below to login to your account
 					</p>
@@ -71,17 +69,16 @@ export function SignInForm() {
 						name="password"
 						render={({ field }) => (
 							<FormItem>
-								<FormLabel asChild>
-									<div className="flex items-center">
-										<Label htmlFor="password">Password</Label>
-										<a
-											href="#"
-											className="ml-auto text-sm underline-offset-4 hover:underline"
-										>
-											Forgot your password?
-										</a>
-									</div>
-								</FormLabel>
+								<div className="flex items-center">
+									<FormLabel>Password</FormLabel>
+									<a
+										href="#"
+										className="ml-auto text-sm underline-offset-4 hover:underline"
+									>
+										Forgot your password?
+									</a>
+								</div>
+
 								<FormControl>
 									<Input type="password" {...field} />
 								</FormControl>
