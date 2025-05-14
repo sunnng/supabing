fbbc27c2b67db7e00c1d9dd2f74a48999b1e092f
Scy <chengyuan7766@gmail.com>
2025-05-14 16:38:57 +0800

    feat: protect dashboard page and profit sign-in-component

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,26 @@
-export default function DashboardPage() {
-	return <div className="p-6">Dashboard</div>;
+import { auth } from "@/lib/auth"; // path to your Better Auth server instance
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+	let session = null;
+	try {
+		session = await auth.api.getSession({
+			headers: await headers(),
+		});
+	} catch (error) {
+		console.error("Error fetching session:", error);
+		// 可以在这里决定是重定向还是显示错误
+		// 为了安全，通常会重定向到登录页
+		redirect("/sign-in");
+	}
+
+	console.log(session); // 如果上面捕获到错误并重定向了，这行不会执行
+
+	if (!session) {
+		redirect("/sign-in");
+	} else {
+		return <div className="p-6">Dashboard</div>;
+	}
 }
--- src/features/auth/components/sign-in-form.tsx
+++ src/features/auth/components/sign-in-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -8,6 +9,7 @@ import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
 	Form,
 	FormControl,
@@ -18,51 +20,58 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { signIn } from "@/lib/auth-client";
-import { useMutation } from "@tanstack/react-query";
-import { Eye, EyeOff, Loader2 } from "lucide-react";
-import { PasswordInput } from "./password-input";
 
 const formSchema = z.object({
 	email: z.string().email({ message: "Invalid email address" }),
 	password: z.string().min(1, { message: "Password is required" }),
+	remember: z.boolean(),
 });
 
 export function SignInForm() {
 	const [isVisible, setIsVisible] = useState<boolean>(false);
+	const [loading, setLoading] = useState(false);
 
 	const toggleVisibility = () => setIsVisible((prevState) => !prevState);
-
-	const mutation = useMutation({
-		mutationFn: async (values: z.infer<typeof formSchema>) => {
-			const { data, error } = await signIn.email({
-				email: values.email,
-				password: values.password,
-			});
-
-			if (error) {
-				throw new Error(error.message);
-			}
-
-			return data;
-		},
-		onSuccess: (data, variables, context) => {
-			toast.success("Login Success!");
-		},
-		onError: (error) => {
-			toast.error(`Login Failed: ${error.message} !`);
-		},
-	});
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			email: "",
 			password: "",
+			remember: true,
 		},
 	});
 
-	function onSubmit(values: z.infer<typeof formSchema>) {
-		mutation.mutate(values);
+	async function onSubmit({
+		email,
+		password,
+		remember,
+	}: z.infer<typeof formSchema>) {
+		await signIn.email(
+			{
+				email,
+				password,
+				callbackURL: "/dashboard",
+				/**
+				 * remember the user session after the browser is closed.
+				 * @default true
+				 */
+				rememberMe: remember,
+			},
+			{
+				onRequest: () => {
+					setLoading(true);
+				},
+				onSuccess: () => {
+					setLoading(false);
+					toast.success("Login Success!");
+				},
+				onError: (ctx) => {
+					setLoading(false);
+					toast.error(`Login Failed: ${ctx.error.message} !`);
+				},
+			},
+		);
 	}
 
 	return (
@@ -71,7 +80,7 @@ export function SignInForm() {
 				onSubmit={form.handleSubmit(onSubmit)}
 				className="flex flex-col gap-6"
 			>
-				<div className="mb-2 flex flex-col items-start gap-2 text-center">
+				<div className="flex flex-col items-start gap-2 text-center">
 					<h1 className="font-bold text-3xl">Welcome back</h1>
 					<p className="text-balance text-muted-foreground text-sm">
 						Enter your email below to login to your account
@@ -141,40 +150,54 @@ export function SignInForm() {
 						)}
 					/>
 
-					<Button
-						type="submit"
-						className="mt-4 w-full"
-						disabled={mutation.isPending}
-						size="lg"
-					>
-						{mutation.isPending && <Loader2 className="animate-spin" />}
+					<FormField
+						control={form.control}
+						name="remember"
+						render={({ field }) => (
+							<FormItem className="flex items-center gap-2">
+								<FormControl>
+									<Checkbox
+										checked={field.value}
+										onCheckedChange={field.onChange}
+									/>
+								</FormControl>
+								<FormLabel>Remember me</FormLabel>
+							</FormItem>
+						)}
+					/>
+
+					<Button type="submit" className="w-full" disabled={loading} size="lg">
+						{loading && <Loader2 className="animate-spin" />}
 						Sign In
 					</Button>
-
-					<div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-border after:border-t">
-						<span className="relative z-10 bg-background px-2 text-muted-foreground">
-							Or continue with
-						</span>
-					</div>
-
-					<Button variant="outline" className="w-full" size="lg">
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<title>github</title>
-							<path
-								d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-								fill="currentColor"
-							/>
-						</svg>
-						Login with GitHub
-					</Button>
 				</div>
+			</form>
+
+			<div className="mt-4 flex flex-col gap-4">
+				<div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-border after:border-t">
+					<span className="relative z-10 bg-background px-2 text-muted-foreground">
+						Or continue with
+					</span>
+				</div>
+
+				<Button variant="outline" className="w-full" size="lg">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<title>github</title>
+						<path
+							d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+							fill="currentColor"
+						/>
+					</svg>
+					Login with GitHub
+				</Button>
+
 				<div className="text-center text-sm">
 					Don&apos;t have an account?{" "}
 					<Link href="/sign-up" className="underline underline-offset-4">
 						Sign up
 					</Link>
 				</div>
-			</form>
+			</div>
 		</Form>
 	);
 }
